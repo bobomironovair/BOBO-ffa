c120a9b324bc3b1e4de1d5d4c125ca7e8f8bceb6
bobomironovair <bobomironov@gmail.com>
2026-06-06 12:24:02 +0300

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,38 @@
+# Say hello
+print("hello world")
+print("welcome to BOBO FFA")
+print("this is a my business")
+print("I am a BOBO FFA Owner")
+print("I have a Movies")
+
+Item = 'cataction, movie for cats'
+print("I have a " + Item)
+print("I have a " + Item + " for cats")
+print("I have a " + Item + " for music")
+
+def say_hello():
+    print("hello world")
+    print("welcome to BOBO FFA")
+    print("this is a my business")
+    print("I am a BOBO FFA Owner")
+    print("I have a Movies")
+
+def get_internet():
+    pass
+    print("I have a " + Item)
+    print("I have a " + Item + " for cats")
+    print("I have a " + Item + " for music")
+
+number = 10
+if number > 5:
+    print("number is greater than 5")
+else:
+    print("number is less than 5")
+
+try:
+    result = 10 / 0
+except ZeroDivisionError:
+    print("You cannot divide by zero!")
+else:
+    print("Result:", result)
+    
